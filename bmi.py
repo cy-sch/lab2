a62013f0d6adac8eb05b4fde84b7568b8389a2b1
cyrus <cyrushow.25@ichat.sp.edu.sg>
2026-05-05 14:48:12 +0800

    help

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height,weight):
    print("bmi=" +str(bmi))
    if (bmi < 18.5):
      print("under weight")
+     return -1
    elif(bmi>=18.5 and bmi<=25):
       print("normal weight")
+      return 0
    elif (bmi>25):
       print("over weight")  
+      return 1
 calculate_bmi(weight="57",height="1.73")   
